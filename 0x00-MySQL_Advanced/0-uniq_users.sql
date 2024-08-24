ec92bd8f58bd26b5dca237ac391c5575d77a4c3a
Y-admen <yassminash305@gmail.com>
2024-08-25 01:26:43 +0300

    table

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,5 +1,5 @@
+--  creates a table users
 CREAT Table IF Not EXISTS users(
-	--  creates a table users
 	id INT Not NULL AUTO_INCREMENT primary key,
 	email VARCHAR(255) Not NULL unique,
 	name VARCHAR(255),
